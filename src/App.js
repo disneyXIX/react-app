23beef96b31092a3bf96475ba9262d796a78242e
Vitaliy Yarmolovskyi <yarmolovskyi.vitaliy@gmail.com>
2024-12-07 02:02:54 +0200

    MA-3: created new component Dialogs and module.css for it`s.edited css

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css'
 import Header from './components/Header/Header'
 import Nav from './components/Nav/Nav'
 import Profile from './components/Profile/Profile'
-import MyPosts from './components/Profile/MyPosts/MyPosts'
+import Dialogs from './components/Dialogs/Dialogs'
 
 const App = () => {
 	return (
 		<div className='app-wrapper'>
 			<Header />
 			<Nav />
-			<Profile />
+			<div className='app-wrapper-content'>
+				<Dialogs />
+			</div>
+			{/* <Profile /> */}
 		</div>
 	)
 }
