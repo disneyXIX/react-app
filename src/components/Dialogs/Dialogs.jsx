5d30c1b922b3a5e5fd286984e49e8ec5d2f8981a
Vitaliy Yarmolovskyi <yarmolovskyi.vitaliy@gmail.com>
2024-12-10 00:50:03 +0200

    MA-5: big changes,created dialogs page and content

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 import s from './Dialogs.module.css'
 
 const Dialogs = props => {
-	return <div>Dialogs</div>
+	return (
+		<div className={s.dialogs}>
+			<div className={s.dialogsItems}>
+				<div className={s.dialog + ' ' + s.active}>Valeria</div>
+				<div className={s.dialog}>Andre</div>
+				<div className={s.dialog}>Yan</div>
+				<div className={s.dialog}>Yan</div>
+			</div>
+			<div className={s.messages}>
+				<div className={s.message}>I love you,my girl!</div>
+				<div className={s.message}>Hi,bro!</div>
+				<div className={s.message}>Are you sure?</div>
+			</div>
+		</div>
+	)
 }
 
 export default Dialogs
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,15 +4,16 @@ import Post from './Post/Post'
 
 const MyPosts = () => {
 	return (
-		<div>
-			<div>
-				<div>My posts</div>
-				<textarea></textarea>
-				<button className={s.button}>Add</button>
-				<div className='{s.posts}'>
-					<Post messages='Hi,it`s my first post!' likesCounter='22' />
-					<Post messages='Today is the best day in my life' likesCounter='15' />
-				</div>
+		<div className={s.posts}>
+			<div className={s.postsTitle}>My posts</div>
+			<textarea
+				className={s.textarea}
+				placeholder="What's on your mind?"
+			></textarea>
+			<button className={s.button}>Add</button>
+			<div className={s.postsList}>
+				<Post messages="Hi, it's my first post!" likesCounter='22' />
+				<Post messages='Today is the best day in my life' likesCounter='15' />
 			</div>
 		</div>
 	)
