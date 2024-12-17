3ca20acf2965b417f89856194ea661ee28bc6aa2
Vitaliy Yarmolovskyi <yarmolovskyi.vitaliy@gmail.com>
2024-12-17 23:12:58 +0200

    MA-17: refactoring FLUX system, rendering

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from '../render'
+let rerenderEntireTree = () => {
+	console.log('state was changed')
+}
 
 let state = {
 	profilePage: {
@@ -25,7 +27,7 @@ let state = {
 	},
 }
 
-export let addPost = () => {
+export const addPost = () => {
 	let newPost = {
 		id: 5,
 		message: state.profilePage.newPostText,
@@ -36,9 +38,12 @@ export let addPost = () => {
 	rerenderEntireTree(state)
 }
 
-export let updateNewPostText = newText => {
+export const updateNewPostText = newText => {
 	state.profilePage.newPostText = newText
 	rerenderEntireTree(state)
 }
 
+export const subscribe = observer => {
+	rerenderEntireTree = observer
+}
 export default state
