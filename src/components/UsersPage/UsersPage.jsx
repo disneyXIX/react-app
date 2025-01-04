42b92aa32f8939f38bfd6e5064cbf2e614963129
Vitaliy Yarmolovskyi <yarmolovskyi.vitaliy@gmail.com>
2025-01-04 02:11:00 +0200

    MA-32: created class component.

--- src/components/UsersPage/UsersPage.jsx
+++ src/components/UsersPage/UsersPage.jsx
@@ -3,63 +3,63 @@ import s from './UsersPage.module.css'
 import axios from 'axios'
 import UserIcon from '../../assets/images/emoji.userlist.png'
 
-const UsersPage = props => {
-	let getUsersList = () => {
-		if (props.users.length === 0) {
-			axios
-				.get('https://social-network.samuraijs.com/api/1.0/users')
-				.then(response => {
-					props.setUsers(response.data.items)
-				})
-		}
+class UsersPage extends React.Component {
+	constructor(props) {
+		super(props)
+		axios
+			.get('https://social-network.samuraijs.com/api/1.0/users')
+			.then(response => {
+				this.props.setUsers(response.data.items)
+			})
 	}
 
-	return (
-		<div>
-			<button onClick={getUsersList}>Load user list</button>
-			{props.users.map(u => (
-				<div key={u.id}>
-					<span>
-						<div className={s.photo}>
-							<img src={u.photos.small != null ? u.photos.small : UserIcon} />
-						</div>
-
-						<div>
-							{u.followed ? (
-								<button
-									onClick={() => {
-										props.unfollow(u.id)
-									}}
-								>
-									Unfollow
-								</button>
-							) : (
-								<button
-									onClick={() => {
-										props.follow(u.id)
-									}}
-								>
-									Follow
-								</button>
-							)}
-						</div>
-					</span>
-
-					<span>
+	render() {
+		return (
+			<div>
+				{this.props.users.map(u => (
+					<div key={u.id}>
 						<span>
-							<div> {u.name}</div>
-							<div> {u.status} </div>
+							<div className={s.photo}>
+								<img src={u.photos.small != null ? u.photos.small : UserIcon} />
+							</div>
+
+							<div>
+								{u.followed ? (
+									<button
+										onClick={() => {
+											this.props.unfollow(u.id)
+										}}
+									>
+										Unfollow
+									</button>
+								) : (
+									<button
+										onClick={() => {
+											this.props.follow(u.id)
+										}}
+									>
+										Follow
+									</button>
+								)}
+							</div>
 						</span>
 
 						<span>
-							<div> {'u.location.country'}</div>
-							<div> {'u.location.city'}</div>
+							<span>
+								<div> {u.name}</div>
+								<div> {u.status} </div>
+							</span>
+
+							<span>
+								<div> {'u.location.country'}</div>
+								<div> {'u.location.city'}</div>
+							</span>
 						</span>
-					</span>
-				</div>
-			))}
-		</div>
-	)
+					</div>
+				))}
+			</div>
+		)
+	}
 }
 
 export default UsersPage
