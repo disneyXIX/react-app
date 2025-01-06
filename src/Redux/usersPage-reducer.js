f24821dc2d1baa73d7fc03112adcaf92da7b8ba2
Vitaliy Yarmolovskyi <yarmolovskyi.vitaliy@gmail.com>
2025-01-06 03:26:17 +0200

    MA-33: pagination.

--- src/Redux/usersPage-reducer.js
+++ src/Redux/usersPage-reducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+const SET_TOTAL_USER_COUNT = 'SET_TOTAL_USER_COUNT'
 
 let initialState = {
 	users: [],
+	pageSize: 1400,
+	totalUserCount: 0,
+	currentPage: 1,
 }
 
 const usersPageReducer = (state = initialState, action) => {
@@ -33,6 +38,15 @@ const usersPageReducer = (state = initialState, action) => {
 		case SET_USERS: {
 			return { ...state, users: action.users }
 		}
+
+		case SET_CURRENT_PAGE: {
+			return { ...state, currentPage: action.currentPage }
+		}
+
+		case SET_TOTAL_USER_COUNT: {
+			return { ...state, totalUserCount: action.totalCount }
+		}
+
 		default:
 			return state
 	}
@@ -43,5 +57,15 @@ export const followAC = userID => ({ type: FOLLOW, userID })
 export const unfollowAC = userID => ({ type: UNFOLLOW, userID })
 
 export const setUsersAC = users => ({ type: SET_USERS, users })
+
+export const setCurrentPageAC = currentPage => ({
+	type: SET_CURRENT_PAGE,
+	currentPage,
+})
+
+export const setTotalUserCountAC = totalCount => ({
+	type: SET_TOTAL_USER_COUNT,
+	totalCount,
+})
 
 export default usersPageReducer
--- src/components/UsersPage/UsersPageContainer.jsx
+++ src/components/UsersPage/UsersPageContainer.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import UsersPage from './UsersPage'
 import { connect } from 'react-redux'
-import { followAC, setUsersAC, unfollowAC } from '../../Redux/usersPage-reducer'
+import {
+	followAC,
+	setUsersAC,
+	unfollowAC,
+	setCurrentPageAC,
+	setTotalUserCountAC,
+} from '../../Redux/usersPage-reducer'
 
 let mapStateToProps = state => {
 	return {
 		users: state.usersPage.users,
+		pageSize: state.usersPage.pageSize,
+		totalUserCount: state.usersPage.totalUserCount,
+		currentPage: state.usersPage.currentPage,
 	}
 }
 
@@ -19,6 +28,12 @@ let mapDispatchToProps = dispatch => {
 		},
 		setUsers: users => {
 			dispatch(setUsersAC(users))
+		},
+		setCurrentPage: pageNumber => {
+			dispatch(setCurrentPageAC(pageNumber))
+		},
+		setTotalUserCount: totalCount => {
+			dispatch(setTotalUserCountAC(totalCount))
 		},
 	}
 }
