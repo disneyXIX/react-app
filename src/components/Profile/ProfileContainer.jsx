16016c4b9a72dfdcd88a952c056c91b3e7ad3880
Vitaliy Yarmolovskyi <yarmolovskyi.vitaliy@gmail.com>
2025-01-07 23:24:58 +0200

    MA-37: self-made withRouter with Hooks.

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,11 +3,24 @@ import Profile from './Profile'
 import axios from 'axios'
 import { connect } from 'react-redux'
 import { setUserProfile } from '../../Redux/profile-reducer'
+import { useParams } from 'react-router-dom'
+
+export function withRouter(Component) {
+	return props => {
+		const params = useParams()
+		return <Component {...props} match={{ params }} />
+	}
+}
 
 class ProfileContainer extends React.Component {
 	componentDidMount() {
+		let userId = this.props.match.params.userID
+
+		if (!userId) {
+			userId = 32015
+		}
 		axios
-			.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+			.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
 			.then(response => {
 				this.props.setUserProfile(response.data)
 			})
@@ -22,4 +35,8 @@ let mapStateToProps = state => ({
 	profile: state.profilePage.profile,
 })
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer)
+let WithUrlDataContainerComponent = withRouter(ProfileContainer)
+
+export default connect(mapStateToProps, { setUserProfile })(
+	WithUrlDataContainerComponent
+)
--- src/components/UsersPage/UsersPage.jsx
+++ src/components/UsersPage/UsersPage.jsx
@@ -32,7 +32,7 @@ let UsersPage = props => {
 				<div key={u.id}>
 					<span>
 						<div className={s.photo}>
-							<NavLink to={'/profile' + u.id}>
+							<NavLink to={'/profile/' + u.id}>
 								<img src={u.photos.small != null ? u.photos.small : UserIcon} />
 							</NavLink>
 						</div>
