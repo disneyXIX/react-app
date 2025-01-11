1f5dc38109cf85e6af85d806a130360151916e3f
Vitaliy Yarmolovskyi <yarmolovskyi.vitaliy@gmail.com>
2025-01-11 16:53:03 +0200

    MA-38: loginization

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import Header from './components/Header/Header'
+
 import Nav from './components/Nav/Nav'
 import { Route, Routes } from 'react-router-dom'
 import News from './components/News/News'
@@ -9,11 +9,12 @@ import Settings from './components/Settings/Settings'
 import DialogsContainer from './components/Dialogs/DialogsContainer'
 import UsersPageContainer from './components/UsersPage/UsersPageContainer'
 import ProfileContainer from './components/Profile/ProfileContainer'
+import HeaderContainer from './components/Header/HeaderContainer'
 
 const App = props => {
 	return (
 		<div className='app-wrapper'>
-			<Header />
+			<HeaderContainer />
 			<Nav />
 			<div className='app-wrapper-content'>
 				<Routes>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import s from './Header.module.css'
+import { NavLink } from 'react-router-dom'
 
-const Header = () => {
+const Header = props => {
 	return (
 		<header className={s.header}>
 			<img src='https://t4.ftcdn.net/jpg/04/96/47/13/360_F_496471319_DbtjoUvKqyy2e9OfgBnK5mm2AXhKpa9m.jpg' />
+			<div className={s.loginBlock}>
+				{props.isAuth ? (
+					<a>{props.login}</a>
+				) : (
+					<NavLink to={'/signIn'}>
+						Sign in<NavLink to={'/signUp'}>or Sign up</NavLink>
+					</NavLink>
+				)}
+			</div>
 		</header>
 	)
 }
